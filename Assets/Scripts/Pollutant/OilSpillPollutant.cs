617ae10df294b53bf1d68694c82b9a43b4c1a563
Akaidus <nikolajjensen6854@gmail.com>
2022-11-22 15:06:04 +0100

    100 Pollution scripts der ikke virker
    Christian prøvede :)

---
+++ Assets/Scripts/Pollutant/OilSpillPollutant.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class OilSpillPollutant : PollutantBase
+{
+    protected override void OnDerivedMouseDown()
+    {
+        pollutantData.ResourceCount += 1;
+        RaisePollutantDataUpdated();
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Pollutant/PollutantBase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public abstract class PollutantBase : MonoBehaviour
+{
+    [SerializeField] protected PollutantData pollutantData;
+    [SerializeField] protected bool playerInVicinity;
+
+    public static event Action<PollutantData> OnPollutantDataUpdated;
+
+    public bool PlayerInVicinity { set => playerInVicinity = value; }
+
+
+    void OnMouseDown()
+    {
+        OnDerivedMouseDown();
+    }
+
+    protected abstract void OnDerivedMouseDown();
+
+    protected void RaisePollutantDataUpdated()
+    {
+        pollutantData.ResourceCount += 1;
+        OnPollutantDataUpdated?.Invoke(pollutantData);
+    }
+}
